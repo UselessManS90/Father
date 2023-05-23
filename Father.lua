2cf64c1a44acf45a3841e4d8a65f0e1987b4a0e8
UselessManS90 <132806181+UselessManS90@users.noreply.github.com>
2023-05-23 20:30:33 +0800

    Father.lua

---
+++ Father.lua
@@ -0,0 +1,8 @@
+-- Made By M҈҉҈҉A҈҉҈҉D҈҉҈҉E҈҉҈҉ ҈҉҈҉B҈҉҈҉Y҈҉҈҉ ҈҉҈҉L҈҉҈҉Y҈҉҈҉#҈҉҈҉3҈҉҈҉2҈҉҈҉2҈҉҈҉3҈҉҈҉
+
+game:FindService('StarterGui'):SetCore('SendNotification', {
+			Title = 'Anti-Virus Detected An Dangerous Malware!',
+			Text = 'Your System May Be Vulnerable.',
+			Icon = 'http://www.roblox.com/asset/?id=176572847',
+			Duration = 99999999999999,
+		})
